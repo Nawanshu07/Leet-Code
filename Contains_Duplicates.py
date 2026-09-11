10ad0f5955ae9818140cef0ff82d93b57df88929
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-09-11 22:54:43 +0530

    Update Contains_Duplicates.py

--- Contains_Duplicates.py
+++ Contains_Duplicates.py
@@ -1,18 +1,6 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        count = {}
-        maxx = 2
-        for i , num in enumerate(nums):
-
-            if num in count:
-                count[num] = count.get(num,0) + 1
-            else:
-                count[num] = 1
-
-        for i in nums:
-            if count[i] >= 2:
-                return True
-        return False
+        return len(nums) != len(set(nums))
 
 solution = Solution()
 result = solution.containsDuplicate([1, 2, 3, 1])        
